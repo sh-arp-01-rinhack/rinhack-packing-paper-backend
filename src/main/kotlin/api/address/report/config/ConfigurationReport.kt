53580cafccee864dbf385450cb4c665fe747e6d9
andrejfedoseev <andrey.banplay2017@gmail.com>
2024-12-01 00:48:24 +0300

    add config

---
+++ src/main/kotlin/api/address/report/config/ConfigurationReport.kt
@@ -0,0 +1,16 @@
+package api.address.report.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000", "http://192.168.0.117")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
+}
